754e4181770525d4d8793c621a280bd475074ee2
jaeyong20 <167734318+jaeyong20@users.noreply.github.com>
2025-02-24 22:03:39 +0900

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MyToken is ERC20, ERC20Permit {
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+}
+
+git init
+git add .
+git commit -m "Initial commit with ERC-20 token contract"
